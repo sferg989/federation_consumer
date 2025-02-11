cc6b2ed36839ca8e51da55d8b72b7c303bfe5646
Stephen Ferguson <steve.ferguson@uscca.com>
2025-02-11 14:58:27 -0700

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 // The remote component provided by federation_provider
 import ProviderButton from 'federation_provider/button';
+import ProviderGradientHeader from 'federation_provider/header';
 
 const App = () => {
   return (
     <div className="content">
+      <ProviderGradientHeader />
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
       <div>
