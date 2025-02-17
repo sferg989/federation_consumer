95020832ebc6f96581d17bb8e512aa279b8467d3
Stephen Ferguson <steve.ferguson@uscca.com>
2025-02-17 15:52:19 -0700

    ce token

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { withZephyr } from 'zephyr-webpack-plugin';
+
 export default defineConfig({
   tools: {
-     rspack: (config) => {
+    rspack: (config) => {
       config.resolve ||= {};
       config.resolve.alias ||= {};
       config.resolve.alias['@util'] = 'src/util';
-      return withZephyr()(config);
+      return withZephyr({
+        authToken: process.env.ZE_SECRET_TOKEN
+      })(config);
     },
   },
   plugins: [
